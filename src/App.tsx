8d5d4c4a5106dc2684c7e88e4be4214d4875131e
Matthew Foyle <mfoyle93@gmail.com>
2024-03-11 07:56:10 +0100

    fix: packages

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,6 @@ import {
 } from "@dynamic-labs/sdk-react-core";
 
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
-import { AlgorandWalletConnectors } from "@dynamic-labs/algorand";
-import { SolanaWalletConnectors } from "@dynamic-labs/solana";
-import { FlowWalletConnectors } from "@dynamic-labs/flow";
-import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
-import { CosmosWalletConnectors } from "@dynamic-labs/cosmos";
-import { MagicWalletConnectors } from "@dynamic-labs/magic";
-import { BloctoEvmWalletConnectors } from "@dynamic-labs/blocto-evm";
 
 function App() {
   return (
@@ -22,17 +14,7 @@ function App() {
         <DynamicContextProvider
           settings={{
             environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
-            walletConnectors: [
-              EthereumWalletConnectors,
-              ZeroDevSmartWalletConnectors,
-              AlgorandWalletConnectors,
-              SolanaWalletConnectors,
-              FlowWalletConnectors,
-              StarknetWalletConnectors,
-              CosmosWalletConnectors,
-              MagicWalletConnectors,
-              BloctoEvmWalletConnectors,
-            ],
+            walletConnectors: [EthereumWalletConnectors],
           }}
         >
           <DynamicWidget />
